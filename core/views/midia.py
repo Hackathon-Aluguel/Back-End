121cd62f5d818dd6552a653ea8f79fb7a1a3093a
erickChavesFiamoncini <erickechaves@gmail.com>
2025-08-29 11:56:32 -0300

    adiciona permissões de acesso e validações nas views de Midia e User

--- core/views/midia.py
+++ core/views/midia.py
@@ -2,13 +2,26 @@ from rest_framework.viewsets import ModelViewSet
 from core.models.midia import Midia, Midia_item
 from core.serializers.midia import MidiaSerializer, Midia_itemSerializer
 
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+
 
 class MidiaViewSet(ModelViewSet):
     queryset = Midia.objects.all()
     serializer_class = MidiaSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save()
 
 class Midia_itemViewSet(ModelViewSet):
     queryset = Midia_item.objects.all()
     serializer_class = Midia_itemSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        item = serializer.validated_data.get("item")
+        if item.usuario != self.request.user:
+            raise PermissionDenied("Você não pode adicionar mídia a itens de outro usuário.")
+        serializer.save()
 
 
--- core/views/user.py
+++ core/views/user.py
@@ -3,12 +3,12 @@ from rest_framework.viewsets import ModelViewSet
 from core.models import User
 from core.serializers.user import userSerializer
 
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 class UserViewSet(ModelViewSet):
     queryset = User.objects.all()
     serializer_class = userSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticatedOrReadOnly]
 
     def get_queryset(self):
         user = self.request.user
@@ -21,4 +21,10 @@ class UserViewSet(ModelViewSet):
         elif user.role == 'padrao':
             return User.objects.filter(id=user.id)
         return User.objects.none()
+
+        def perform_update(self, serializer):
+        user = self.get_object()
+        if self.request.user != user:
+            raise PermissionDenied("Você só pode alterar sua própria foto de perfil.")
+        serializer.save()
 
