a1a6e4a05ea0ee33e1e4be37dcae2ce552e2e29e
mateusLimadeMacedo <mateuslimamacedo2008@gmail.com>
2025-10-09 08:49:24 -0300

    sepe chat ultima

--- a_users/serializers.py
+++ a_users/serializers.py
@@ -10,4 +10,4 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'name', 'avatar', 'info']
+        fields = ['id', 'username', 'email', 'name', 'avatar', 'info']
